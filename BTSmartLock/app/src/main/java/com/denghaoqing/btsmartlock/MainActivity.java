f6d1b60f48e5a56ee160789f648f2ff4916904f9
邓昊晴 <dhq.sunny@gmail.com>
2016-10-21 23:49:34 +0800

    Initial commit of the Android App
    With basic framework constructed

---
+++ BTSmartLock/app/src/main/java/com/denghaoqing/btsmartlock/MainActivity.java
@@ -0,0 +1,107 @@
+package com.denghaoqing.btsmartlock;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.denghaoqing.btsmartlock.fragments.UnlockPageFragment;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    public static final int MSG_AUTH_SUCCESS = 100;
+    public static final int MSG_AUTH_FAILED = 101;
+    public static final int MSG_AUTH_ERROR = 102;
+    public static final int MSG_AUTH_HELP = 103;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+            this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment;
+        switch(id){
+            case R.id.nav_login:
+                Intent mIntent =new Intent(this,LoginActivity.class);
+                startActivity(mIntent);
+                break;
+            case R.id.nav_unlock:
+                fragment = UnlockPageFragment.newInstances();
+                fragmentManager.beginTransaction().replace(R.id.coo_homepage_content, fragment).commit();
+                break;
+            case R.id.nav_settings:
+                break;
+            case R.id.nav_logout :
+                break;
+            default:
+                break;
+        }
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ BTSmartLock/app/src/main/java/com/denghaoqing/btsmartlock/fragments/UnlockPageFragment.java
@@ -0,0 +1,217 @@
+package com.denghaoqing.btsmartlock.fragments;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.ContactsContract;
+import android.support.v4.os.CancellationSignal;
+import android.support.v4.app.Fragment;
+import android.support.v4.hardware.fingerprint.FingerprintManagerCompat;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.denghaoqing.btsmartlock.CryptoObjectHelper;
+import com.denghaoqing.btsmartlock.MainActivity;
+import com.denghaoqing.btsmartlock.FingerprintAuthCallBack;
+import com.denghaoqing.btsmartlock.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link UnlockPageFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link UnlockPageFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class UnlockPageFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private boolean isFingerPrint=true;
+
+    private ImageView imgFinger;
+    private ImageView imgSecureAuthStat;
+    private ImageView imgCommStat;
+    private ImageView imgAuthStat;
+    private TextView txtTips;
+
+
+    private FingerprintManagerCompat fingerprintManager = null;
+    private FingerprintAuthCallBack myAuthCallback = null;
+    private CancellationSignal cancellationSignal = null;
+
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private Handler handler = null;
+    private OnFragmentInteractionListener mListener;
+
+
+    public static UnlockPageFragment newInstances() {
+        UnlockPageFragment fragment = new UnlockPageFragment();
+        Bundle bundle = new Bundle();
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+    public UnlockPageFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment UnlockPageFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static UnlockPageFragment newInstance(String param1, String param2) {
+        UnlockPageFragment fragment = new UnlockPageFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+        //imgFinger = (ImageView)View.
+
+        handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+
+                Log.d("FingerPrint", "msg: " + msg.what + " ,arg1: " + msg.arg1);
+                switch (msg.what) {
+                    case MainActivity.MSG_AUTH_SUCCESS:
+                        Log.d("FingerPrint", "msg: " + msg.what + " ,arg1: " + msg.arg1);
+                        imgFinger.setImageResource( R.drawable.ic_verified_user_black_24dp) ;
+                        imgSecureAuthStat.setVisibility(View.VISIBLE);
+                        txtTips.setText(R.string.finger_auth_success_tips);
+                        Toast.makeText(getContext(), "Auth Success!", Toast.LENGTH_SHORT).show();
+
+                        break;
+                    case MainActivity.MSG_AUTH_FAILED:
+                        txtTips.setText(R.string.finger_auth_failure_tips);
+                        imgFinger.setImageResource(R.drawable.ic_info_outline_black_24dp);
+                        Toast.makeText(getContext(), "Auth Failed!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case MainActivity.MSG_AUTH_ERROR:
+
+                        break;
+                    case MainActivity.MSG_AUTH_HELP:
+
+                        break;
+                }
+            }
+        };
+
+
+        // init fingerprint.
+        fingerprintManager = FingerprintManagerCompat.from(getContext());
+
+        if (!fingerprintManager.isHardwareDetected()) {
+
+        } else if (!fingerprintManager.hasEnrolledFingerprints()) {
+            // no fingerprint image has been enrolled.
+
+        } else {
+            try {
+                myAuthCallback = new FingerprintAuthCallBack(handler) ;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+        try {
+            CryptoObjectHelper cryptoObjectHelper = new CryptoObjectHelper();
+            if (cancellationSignal == null) {
+                cancellationSignal = new CancellationSignal();
+            }
+            fingerprintManager.authenticate(cryptoObjectHelper.buildCryptoObject(), 0,
+                    cancellationSignal, myAuthCallback, null);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getContext(), "Fingerprint init failed! Try again!", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_unlock_page, container, false);
+        imgFinger =(ImageView) view.findViewById(R.id.fingerprintimg);
+        imgSecureAuthStat = (ImageView)view.findViewById(R.id.img_stat_security);
+        imgCommStat=(ImageView)view.findViewById(R.id.img_stat_remote);
+        imgAuthStat=(ImageView)view.findViewById(R.id.img_stat_auth);
+        txtTips = (TextView)view.findViewById(R.id.txtTips);
+        imgFinger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isFingerPrint) {
+                    imgFinger.setImageResource(R.drawable.ic_lock_outline_black_24dp);
+                    txtTips.setText(R.string.pin_auth_tips);
+                    isFingerPrint = false;
+                }else{
+                    imgFinger.setImageResource(R.drawable.ic_fingerprint_black_24dp);
+                    txtTips.setText(R.string.finger_auth_tips);
+                    isFingerPrint = true;
+                }
+            }
+        });
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
